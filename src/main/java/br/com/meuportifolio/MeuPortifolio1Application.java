14339247ec4efd19e437d925561e607ca0daf1af
cr4ck3r123 <suportewicell@gmail.com>
2019-07-01 16:50:50 -0300

    Primeiro commit - Meu portifolio

---
+++ src/main/java/br/com/meuportifolio/MeuPortifolio1Application.java
@@ -0,0 +1,13 @@
+package br.com.meuportifolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPortifolio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPortifolio1Application.class, args);
+	}
+
+}
